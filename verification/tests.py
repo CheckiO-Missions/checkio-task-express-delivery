d3d220fd78a27fa8f140780bfa7836d776914cc2
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-11-17 12:28:14 +1200

    tests

--- verification/tests.py
+++ verification/tests.py
@@ -8,7 +8,7 @@ Each test is dict with
 """
 
 TESTS = {
-    "Basics": [
+    "1. Basics": [
         {
             "input": ["S...", "....", "B.WB", "..WE"],
             "answer": (["S...", "....", "B.WB", "..WE"], 12),
@@ -20,16 +20,114 @@ TESTS = {
             "explanation": "DDBLLLBD"
         }
     ],
-    #"Extra": [
-    #    {
-    #        "input": [6, 3],
-    #        "answer": 9,
-    #        "explanation": "6+3=?"
-    #    },
-    #    {
-    #        "input": [6, 7],
-    #        "answer": 13,
-    #        "explanation": "6+7=?"
-    #    }
-    #]
+    "2. Extra": [
+        {
+            "input": ["S.W...", "..WB..", "..WW..", "....B.", "....W.", "..B.BE"],
+            "answer": (["S.W...", "..WB..", "..WW..", "....B.", "....W.", "..B.BE"], 20),
+            "explanation": ""
+        },
+        {
+            "input": ["SBW...", ".WWB..", "..WW..", "......", "...WW.", "..BWBE"],
+            "answer": (["SBW...", ".WWB..", "..WW..", "......", "...WW.", "..BWBE"], 18),
+        },
+        {
+            "input": ["S..BW.", ".WWWWW", ".W....", ".W.W.B", ".W.W..", "...W.E"],
+            "answer": (["S..BW.", ".WWWWW", ".W....", ".W.W.B", ".W.W..", "...W.E"], 29),
+        },
+    ],
+    "3. Weird": [
+        {
+            "input": ["SB....BE"],
+            "answer": (["SB....BE"], 11),
+        },
+        {
+            "input": ["S...BB..E"],
+            "answer": (["S...BB..E"], 16),
+        },
+
+        {
+            "input": ["SBBBBB",
+                      "BBBBBB",
+                      "BBBBBB",
+                      "BBBBBB",
+                      "BBBBBE"],
+            "answer": (["SBBBBB",
+                        "BBBBBB",
+                        "BBBBBB",
+                        "BBBBBB",
+                        "BBBBBE"], 13),
+        },
+        {
+            "input": ["S.......",
+                      "........",
+                      "..B.....",
+                      ".....B..",
+                      "........",
+                      ".......E"],
+            "answer": (["S.......",
+                        "........",
+                        "..B.....",
+                        ".....B..",
+                        "........",
+                        ".......E"], 22),
+        },
+        {
+            "input": ["SW.B.W.",
+                      ".W.W.W.",
+                      ".W.W.W.",
+                      ".W.W.WB",
+                      ".W.W.W.",
+                      ".B.W..E"],
+            "answer": (["SW.B.W.",
+                        ".W.W.W.",
+                        ".W.W.W.",
+                        ".W.W.WB",
+                        ".W.W.W.",
+                        ".B.W..E"], 35),
+        },
+
+    ],
+    "4. Big": [
+        {
+            "input": ["S..BW....",
+                      "...W.....",
+                      "..W......",
+                      ".........",
+                      ".WWWWWWW.",
+                      "..WB.....",
+                      "..W....B.",
+                      "..WWW.WWW",
+                      "....W...E"],
+            "answer": (["S..BW....",
+                        "...W.....",
+                        "..W......",
+                        ".........",
+                        ".WWWWWWW.",
+                        "..WB.....",
+                        "..W....B.",
+                        "..WWW.WWW",
+                        "....W...E"], 38),
+        },
+        {
+            "input": ["S........",
+                      ".WWW.WWW.",
+                      ".W.B...W.",
+                      ".WWWWWWW.",
+                      ".W..B..W.",
+                      ".W.WWW.WW",
+                      ".W...W.WB",
+                      ".WWW.W.W.",
+                      ".....W..E"],
+            "answer": (["S........",
+                        ".WWW.WWW.",
+                        ".W.B...W.",
+                        ".WWWWWWW.",
+                        ".W..B..W.",
+                        ".W.WWW.WW",
+                        ".W...W.WB",
+                        ".WWW.W.W.",
+                        ".....W..E"], 56),
+        },
+
+    ]
 }
