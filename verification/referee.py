c71fcb7f26a808e4aacdca700bd9e219088e236d
Bryukhanov Valentin <bvv.mag@gmail.com>
2016-09-28 13:19:30 +0300

    Fix referee
    The checking of the existence of coordinates (inside the field map) was wrong. As the result, it was possible to get indexError. Also I reformatted (a little) the expression for readability

--- verification/referee.py
+++ verification/referee.py
@@ -41,7 +41,7 @@ def checker(answer, route):
                 continue
         n_row, n_col = s_row + ACTIONS[step][0], s_col + ACTIONS[step][1],
         total_time += 2 if hold_box else 1
-        if 0 > n_row or n_row >= max_row or 0 > n_col or n_row >= max_col:
+        if n_row < 0 or n_row >= max_row or n_col < 0 or n_col >= max_col:
             return False, "We've lost Stephan."
         if field[n_row][n_col] == "W":
             return False, "Stephan fell in water."
